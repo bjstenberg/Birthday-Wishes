b612dc73eb9f3be53f3d785776c738d0f12c6b90
hypicks <44602385+bjstenberg@users.noreply.github.com>
2022-05-08 21:50:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+##################### Normal Starting Project ######################
+import pandas
+import random
+import smtplib
+from datetime import datetime
+
+MY_EMAIL = "stenberg.p.b@gmail.com"
+MY_PASSWORD = "##ThebigbadbearGmail89"
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+# HINT 2: Use pandas to read the birthdays.csv
+data = pandas.read_csv("birthdays.csv")
+# HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
+# Dictionary comprehension template for pandas DataFrame looks like this:
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+# HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
+# 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"Letters/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        # Have to send the contents to a variable to avoid bugs.
+        contents = contents.replace("[NAME]", birthday_person["name"])
+    # 4. Send the letter generated in step 3 to that person's email address.
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n{contents}".encode("utf-8")
+        )
+
+
+
+
+
